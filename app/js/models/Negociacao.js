ad674969c9cb9a539193ca0e1a7c0384f718a98c
Leonardo Medeiros <>
2019-06-30 15:52:47 -0300

    first commit

---
+++ app/js/models/Negociacao.js
@@ -0,0 +1,21 @@
+class Negociacao {
+    constructor(data, quantidade, valor) {
+        // _ representa uma convenção que diz que esse tipo de variável
+        // não pode ser alterado fora dos métodos da própria classe
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+    get data() {
+        return this._data;
+    }
+    get quantidade() {
+        return this._quantidade;
+    }
+    get valor() {
+        return this._valor;
+    }
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+}
